ffe5066d07bc4564f51ab9075037f4372e967970
YamanAddas <101920070+YamanAddas@users.noreply.github.com>
2026-02-11 03:38:32 -0500

    Delete version.js

--- version.js
+++
@@ -1,6 +0,0 @@
-// os/version.js
-// Single source of truth for the current YamanOS version.
-// Keep this small and dependency-free.
-export const VERSION = 'v1.0.0';
-export const BUILD = 'v1.0.0';
-export const ASSET_VERSION = 'v1.0.0';
